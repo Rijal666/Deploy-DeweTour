1926c714db799bb2905162183f2a7d9a66e12254
Rijal666 <rizkirizalmualim2@gmail.com>
2023-07-04 16:19:34 +0700

    responsive home

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
